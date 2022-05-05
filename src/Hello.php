7b462609e0abc6782aada26903ca852abb9798b7
kimhuanle <kimhuanle@gmail.com>
2022-05-04 22:35:14 -0400

    Initial Commit

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) Huan Le <kimhuanle@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Lehk\WeatherPackage;
+
+class Hello
+{
+    public function say($name)
+    {
+        return "Hello, $name!";
+    }
+}
+
+?>
